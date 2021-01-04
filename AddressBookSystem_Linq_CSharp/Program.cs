fc372a9e2b41579bf01c5505ee4dce01e4ec2c5f
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-01-04 22:36:57 +0530

    [NIJAM] Add:Create DataTable To store Contacts

--- AddressBookSystem_Linq_CSharp/Program.cs
+++ AddressBookSystem_Linq_CSharp/Program.cs
@@ -7,7 +7,9 @@ namespace AddressBookSystem_Linq_CSharp
         static void Main(string[] args)
         {
             Console.WriteLine("***Welcome_To_AddressBookSystem_Linq***");
-
+            AddrssBookModel model = new AddrssBookModel();
+            AddressBookDataTable dataTable = new AddressBookDataTable();
+            dataTable.CreateTable(model);
         }
     }
 }
