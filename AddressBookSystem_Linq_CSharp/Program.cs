9caac34e9853120c12baf0013f0ae7846512f47c
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-01-05 09:52:28 +0530

    [NIJAM] Add:Retrive Contacts From paricular City or State.

--- AddressBookSystem_Linq_CSharp/Program.cs
+++ AddressBookSystem_Linq_CSharp/Program.cs
@@ -8,12 +8,12 @@ namespace AddressBookSystem_Linq_CSharp
         {
             Console.WriteLine("***Welcome_To_AddressBookSystem_Linq***");
             AddressBookModel model = new AddressBookModel();
-            AddressBookDataTable dataTable = new AddressBookDataTable();
-            dataTable.CreateTable(model);
+            AddressBookDataTable adddataTable = new AddressBookDataTable();
+            adddataTable.CreateTable(model);
             
             while (true)
             {
-                Console.WriteLine("\nEnter Choice  \n1. Add Contact \n2. Display \n3. Edit Contact  \n4. DeletContact \n5. Exit ");
+                Console.WriteLine("\nEnter Choice  \n1. Add Contact \n2. Display \n3. Edit Contact  \n4. DeletContact \n5. ContactFrom_City \n6. ContactsFrom_State \n7.Exit ");
                 int choise = Convert.ToInt32(Console.ReadLine());
                 try
                 {
@@ -36,10 +36,10 @@ namespace AddressBookSystem_Linq_CSharp
                             model.Phone_Number = Console.ReadLine();
                             Console.Write("Enter the E-mail : ");
                             model.Email = Console.ReadLine();
-                            dataTable.AddContact(model);
+                            adddataTable.AddContact(model);
                             break;
                         case 2:
-                            dataTable.Display();
+                            adddataTable.Display();
                             break;
                         case 3:
                             Console.Write("Enter the first name: ");
@@ -58,18 +58,30 @@ namespace AddressBookSystem_Linq_CSharp
                             model.Phone_Number = Console.ReadLine();
                             Console.Write("Enter the E-mail : ");
                             model.Email = Console.ReadLine();
-                            dataTable.EditContact(model);
+                            adddataTable.EditContact(model);
                             break;
                         case 4:
                             Console.Write("Enter First Name: ");
                             model.First_Name = Console.ReadLine();
-                            dataTable.DeleteContact(model);
+                            adddataTable.DeleteContact(model);
                             break;
                         case 5:
+                            Console.WriteLine("Enter the city :");
+                            model.City = Console.ReadLine();
+                            adddataTable.RetrievePersonByUsingCity(model);
+                            Console.WriteLine("Contacts_From_City");
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter the State : ");
+                            model.State = Console.ReadLine();
+                            adddataTable.RetrievePersonByUsingState(model);
+                            Console.WriteLine("Contacts_From_State");
+                            break;
+                        case 7:
                             Environment.Exit(0);
                             break;
                         default:
-                            Console.WriteLine("Enter The Valid Choise");
+                            Console.WriteLine("Invalid option....");
                             break;
                     }
                 }
